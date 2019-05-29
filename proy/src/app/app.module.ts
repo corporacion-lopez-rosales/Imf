f28aa276738237acba75dcd280760b7c0de05ff9
Anaksasuke <jose_raya23@hotmail.com>
2019-05-29 15:21:28 -0500

    Home

--- proy/src/app/app.module.ts
+++ proy/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LoginEntroComponent } from './login-entro/login-entro.component';
 import {RouterModule}from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
 import {HttpClientModule} from '@angular/common/http';
 import { RegistrarComponent } from './registrar/registrar.component';
 import { FormsModule }    from '@angular/forms';
+import {MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatToolbarModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -21,8 +22,8 @@ import { FormsModule }    from '@angular/forms';
     RegistrarComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule,BrowserAnimationsModule,
+    FormsModule,MatButtonModule, MatNativeDateModule, MatIconModule, MatSidenavModule, MatListModule, MatToolbarModule,
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot([
--- proy/src/app/home/home.component.ts
+++ proy/src/app/home/home.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy ,OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) { 
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
   ngOnInit() {
   }
 
